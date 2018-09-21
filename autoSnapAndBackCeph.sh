9408901363191726355cdb20da0fbd75f4742d61
root <root@pve1.doop.fi>
2018-09-21 12:03:33 +0300

    only backup running

--- autoSnapAndBackCeph.sh
+++ autoSnapAndBackCeph.sh
@@ -3,5 +3,10 @@
 vms=`grep 'ceph:' /etc/pve/nodes/*/qemu-server/* | egrep -v 'backup=0|unused' | awk -F'/' '{print $7}' | sed 's/\..*//' | uniq`
 for vm in $vms
 do
- ./snapANDback.sh $vm
+ STATUS=$(qm status $vm)
+ if [ "$STATUS" == "status: running"]; then
+    ./snapANDback.sh $vm
+ else
+  echo "VM $vm $STATUS"
+ fi
 done
